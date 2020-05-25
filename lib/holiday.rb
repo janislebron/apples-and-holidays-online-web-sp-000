3337a5c8a8b05e685e13dc1239126055e8fc4537
Janis Lebron <janislebron@gmail.com>
2020-05-25 00:24:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,23 +1,5 @@
 require 'pry'
 
-def test_h
-{
-     :winter => {
-       :christmas => ["Lights", "Wreath"],
-       :new_years => ["Party Hats"]
-     },
-     :summer => {
-       :fourth_of_july => ["Fireworks", "BBQ"]
-     },
-     :fall => {
-       :thanksgiving => ["Turkey"]
-     },
-     :spring => {
-       :memorial_day => ["BBQ"]
-     }
-   }
-end
-
 def second_supply_for_fourth_of_july(holiday_hash)
   holiday_hash.each do |season, holiday|
     if season == :spring
