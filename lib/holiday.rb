9c1ebe93651bb26822fa0132106a9b7ead1fef78
Janis Lebron <janislebron@gmail.com>
2020-05-25 00:45:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,7 @@ holiday_supplies = {
 
 def second_supply_for_fourth_of_july(holiday_supplies)
   holiday_supplies[:summer][:fourth_of_july][1]
+  binding.pry
 end
 
 second_supply_for_fourth_of_july(holiday_supplies)
